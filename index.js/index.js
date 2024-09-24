065135f1b0db66542381636dbd40b069b41a7839
franci5c0 <cursosenaitecnico@gmail.com>
2024-09-24 19:56:50 -0300

    pegue

--- index.js/index.js
+++ index.js/index.js
@@ -2,26 +2,12 @@
 /*POP-UP*/
 const button = document.querySelector("button");
 const modal = document.querySelector("dialog");
+const buttonClose = document.getElementById("botao");
 
 button.onclick = function () {
     modal.showModal()
 }
 
-
-/*SLIDES CABEÇALHO*/
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-setInterval( function(){
-    nextImage()
-}, 2000)
-
-
-function nextImage() {
- count++;
- if(count>9) {
-    count = 1;
- }
-
- document.getElementById("radio"+count).checked = true
+buttonClose.onclick = function () {
+    modal.close()
 }
