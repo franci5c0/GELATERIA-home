bb4328a48dc74a755496cf7a3ae0a19fa7f65b05
franci5c0 <cursosenaitecnico@gmail.com>
2024-09-24 15:57:03 -0300

    alterações feitas na rede social

--- index.js/index.js
+++ index.js/index.js
@@ -1,6 +1,27 @@
+
+/*POP-UP*/
 const button = document.querySelector("button");
 const modal = document.querySelector("dialog");
 
 button.onclick = function () {
     modal.showModal()
+}
+
+
+/*SLIDES CABEÇALHO*/
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage()
+}, 2000)
+
+
+function nextImage() {
+ count++;
+ if(count>9) {
+    count = 1;
+ }
+
+ document.getElementById("radio"+count).checked = true
 }
