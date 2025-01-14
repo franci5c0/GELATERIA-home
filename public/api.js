babe12c3c4bf940a15c85fea1c65091f393830ab
franci5c0 <cursosenaitecnico@gmail.com>
2025-01-14 11:42:53 -0300

    refazendo

--- public/api.js
+++
@@ -1,5 +0,0 @@
-// Define a URL base da API como 'http://localhost:3000/api'.
-const API_URL = 'http://localhost:3500/api';
-
-
-
--- routes/bolos.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');//importa o framework express
-const router=express.Router();//cria um novo roteado
-const doceriaController=require('../controllers/doceriaController')//importa o controlador das tabelas
-
-//DEFININDO UMA ROTA PARA OBTER TODOS OS BOLOS DA TABELA
-router.get('/', doceriaController.getAllBolos);
-
-//EXPORTANDO O ROTEADOR
-module.exports=router;
